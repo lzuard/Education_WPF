6cabcd4164f923fdeb91c75e6a1c0a6dba3420b5
Izuard <sasha.20133@gmail.com>
2022-07-04 14:05:41 +0300

    Add viewmodel base class

---
+++ EducationWPF/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EducationWPF.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+
+        protected virtual bool Set<T>(ref T failed, T value, [CallerMemberName] string PropertName = null)
+        {
+            if (Equals(failed, value)) return false;
+            failed = value;
+            OnPropertyChanged(PropertName);
+            return true;
+        }
+    }
+}
