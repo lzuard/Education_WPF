0282e91cc18920a47a0d7510a4437a32b3155bc4
Izuard <sasha.20133@gmail.com>
2022-07-04 13:27:40 +0300

    Add test console project

---
+++ Tests/EWPF_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EWPF_Console // Note: actual namespace depends on the project name.
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
