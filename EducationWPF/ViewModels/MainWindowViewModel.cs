a702d607f305344f2030a3ec562eca2469116a33
Izuard <sasha.20133@gmail.com>
2022-07-04 14:36:25 +0300

    Testing main vindow viewmodel

--- EducationWPF/ViewModels/MainWindowViewModel.cs
+++ EducationWPF/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EducationWPF.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace EducationWPF.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel: ViewModel
     {
+        #region Window title
+        private string _Title="Some title";
+
+        /// <summary>Window title</summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title= value;
+            //    OnPropertyChanged();
+            //}
+            set=> Set(ref _Title, value);
+
+        }
+        #endregion
+
     }
 }
