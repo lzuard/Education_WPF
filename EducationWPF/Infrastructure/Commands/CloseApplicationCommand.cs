a2e5b1da7125f66effb47d8ffab6adb115053c70
Izuard <sasha.20133@gmail.com>
2022-07-05 17:42:19 +0300

    Add separate class for exit command

---
+++ EducationWPF/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,16 @@
+﻿using EducationWPF.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EducationWPF.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter)=>true;
+        public override void Execute(object? parameter)=> Application.Current.Shutdown();
+    }
+}
