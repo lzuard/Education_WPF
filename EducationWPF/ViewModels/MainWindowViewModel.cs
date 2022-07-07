03bda263e314cbbb17bb3931c262fc44355aea8d
Izuard <sasha.20133@gmail.com>
2022-07-07 19:08:40 +0300

    Add buttons for navigating between tab items

--- EducationWPF/ViewModels/MainWindowViewModel.cs
+++ EducationWPF/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using EducationWPF.Models;
 using EducationWPF.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Windows;
 using System.Windows.Input;
 using OxyPlot.Wpf;
@@ -11,6 +12,19 @@ namespace EducationWPF.ViewModels
 {
     internal class MainWindowViewModel: ViewModel
     {
+
+        #region SelectedPageIndex:int
+
+        private int _SelectedPageIndex = 0;
+
+        public int SelectedPageIndex
+        {
+            get => _SelectedPageIndex;
+            set => Set(ref _SelectedPageIndex, value);
+        }
+
+        #endregion
+
         #region TestDataPoints: IEnumerable
         private IEnumerable<DataPoint> _TestDataPoints;
 
@@ -62,6 +76,18 @@ namespace EducationWPF.ViewModels
         }
         #endregion
 
+        #region Change Tab Index
+
+        public ICommand ChangeTabIndexCommand { get; }
+        private bool CanChangeTabIndexCommandExecute(object p) => _SelectedPageIndex >= 0;
+
+        private void OnChangeTabIndexCommandExecuted(object p)
+        {
+            if (p is null) return;
+            SelectedPageIndex += Convert.ToInt32(p);
+        }
+        #endregion
+
         #endregion
 
 
@@ -69,6 +95,7 @@ namespace EducationWPF.ViewModels
         {
             #region Commands
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            ChangeTabIndexCommand = new LambdaCommand(OnChangeTabIndexCommandExecuted, CanChangeTabIndexCommandExecute);
             #endregion
 
             var data_points=new List<DataPoint>((int)(360/0.1));
