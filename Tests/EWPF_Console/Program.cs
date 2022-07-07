c5f9942d7bc7ef515b771cbe4c35c60cd5603946
Izuard <sasha.20133@gmail.com>
2022-07-07 16:25:27 +0300

    Revert "Implement http request in the console"
    This reverts commit 7e8f6257d305f0e816ca1cfed63944f450b009ec.

--- Tests/EWPF_Console/Program.cs
+++ Tests/EWPF_Console/Program.cs
@@ -4,19 +4,9 @@ namespace EWPF_Console // Note: actual namespace depends on the project name.
 {
     internal class Programm
     {
-        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-
         static void Main(string[] args)
         {
-            var client = new HttpClient();
-
-            var response = client.GetAsync(data_url).Result;
-            var csv_str = response.Content.ReadAsStringAsync().Result;
-
-
-
-
-            Console.ReadLine();
+            Console.WriteLine("Hello World!");
         }
     }
 }
