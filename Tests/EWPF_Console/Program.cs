deed14fba3bd120373932f01b9ca26ef88a325e3
Izuard <sasha.20133@gmail.com>
2022-07-07 17:38:19 +0300

    Testing buisness logic

--- Tests/EWPF_Console/Program.cs
+++ Tests/EWPF_Console/Program.cs
@@ -11,7 +11,7 @@ namespace EWPF_Console // Note: actual namespace depends on the project name.
 
         private static async Task<Stream> GetDataStream()
         {
-            var client = new HttpClient(); 
+            var client = new HttpClient();
             var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
             return await response.Content.ReadAsStreamAsync();
         }
@@ -24,8 +24,8 @@ namespace EWPF_Console // Note: actual namespace depends on the project name.
             while (!data_reader.EndOfStream)
             {
                 var line = data_reader.ReadLine();
-                if (string.IsNullOrEmpty(line)) continue;
-                yield return line;
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line.Replace("Korea,", "Korea -").Replace("Bonaire,", "Bonaire -");
             }
         }
 
@@ -36,17 +36,34 @@ namespace EWPF_Console // Note: actual namespace depends on the project name.
             .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
 
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
 
+            foreach (var row in lines)
+            {
+                var province=row[0].Trim() ?? String.Empty;
+                var country_name = row[1].Trim(' ', '"') ?? String.Empty;
+                var counts = row.Skip(4).Select(int.Parse).ToArray();
+                yield return (country_name, province, counts);
+            }
+
+        }
 
         static void Main(string[] args)
         {
             //foreach (var data_line in GetDataLines())
             //    Console.WriteLine(data_line);
 
-            var dates = GetDatas();
+            //var dates = GetDatas();
             
-            Console.WriteLine(String.Join("\r\n", dates));
+            //Console.WriteLine(String.Join("\r\n", dates));
 
+            var russia_data = GetData()
+                .First(v=>v.Country.Equals("Russia",StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(string.Join("\r\n", GetDatas().Zip(russia_data.Counts, (date, count) => $"{date:dd:MM} - {count}")));
             Console.ReadLine();
         }
     }
