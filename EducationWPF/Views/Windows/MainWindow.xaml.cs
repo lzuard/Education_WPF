57dd1ef76acdec64f86688868c88a094b86351f9
Izuard <sasha.20133@gmail.com>
2022-07-18 19:39:56 +0300

    Add groups filter

--- EducationWPF/Views/Windows/MainWindow.xaml.cs
+++ EducationWPF/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using EducationWPF.Models.Decanat;
 
 namespace EducationWPF
 {
@@ -10,6 +14,28 @@ namespace EducationWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GroupsCollection_OnFilter(object sender, FilterEventArgs e)
+        {
+            if(!(e.Item is Group group)) return;
+            if(group.Name is null) return;
+
+
+            var filter_text = GroupNameFilterText.Text;
+            if(filter_text.Length==0) return;
+
+            if(group.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if(group.Description != null && group.Description.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void OnGroupsFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var text_box=(TextBox)sender;
+            var collection = (CollectionViewSource) text_box.FindResource("GroupsCollection");
+            collection.View.Refresh();
         }
     }
 }
