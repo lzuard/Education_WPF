d120aa77994258343753fc8d069445255ebf9d3e
Izuard <sasha.20133@gmail.com>
2022-07-19 14:35:04 +0300

    add collection elements sorting and grouping

--- EducationWPF/Views/Windows/MainWindow.xaml.cs
+++ EducationWPF/Views/Windows/MainWindow.xaml.cs
@@ -37,5 +37,6 @@ namespace EducationWPF
             var collection = (CollectionViewSource) text_box.FindResource("GroupsCollection");
             collection.View.Refresh();
         }
+
     }
 }
