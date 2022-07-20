954dc223a209deeb4c749ca24318466bbf33bbea
Izuard <sasha.20133@gmail.com>
2022-07-20 17:32:45 +0300

    Add base logic for statistics viewmodel

--- EducationWPF/ViewModels/CountiesStatisticViewModel.cs
+++ EducationWPF/ViewModels/CountiesStatisticViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using EducationWPF.Infrastructure.Commands;
+using EducationWPF.Models;
 using EducationWPF.Services;
 using EducationWPF.ViewModels.Base;
 
@@ -13,10 +16,44 @@ namespace EducationWPF.ViewModels
         private DataService _DataService;
 
         private MainWindowViewModel MainModel { get; }
+
+
+        #region Countries : IEnymeravle<CountryInfo> 
+
+        private IEnumerable<CountryInfo> _Countries;
+
+        public IEnumerable<CountryInfo> Countries
+        {
+            get => _Countries;
+            private set => Set(ref _Countries, value);
+        }
+
+        #endregion
+
+
+
+        #region Commands
+
+        public ICommand RefreshDataCommand { get; }
+
+        private void OnRefreshDataCommand(object p)
+        {
+            Countries = _DataService.GetData();
+        }
+
+        #endregion
+
+
         public CountiesStatisticViewModel(MainWindowViewModel MainModel)
         {
-            MainModel = MainModel;
+            this.MainModel = MainModel;
             _DataService=new DataService();
+
+            #region Commands
+
+            RefreshDataCommand = new LambdaCommand(OnRefreshDataCommand);
+
+            #endregion
         }
     }
 }
