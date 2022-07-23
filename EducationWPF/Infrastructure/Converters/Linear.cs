a693ac36ce49ed5edd401ecd0f6b83da5b64b00d
Izuard <sasha.20133@gmail.com>
2022-07-24 00:59:00 +0300

    Add multiple converters

---
+++ EducationWPF/Infrastructure/Converters/Linear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace EducationWPF.Infrastructure.Converters
+{
+
+    /// <summary>
+    /// f(x)=K*x+B
+    /// </summary>
+    internal class Linear : Converter
+    {
+        public double K { get; set; } = 1;
+
+        public double B { get; set; }
+
+        public override object Convert(object v, Type t, object p, CultureInfo c)
+        {
+            if (v is null) return null;
+            var x=System.Convert.ToDouble(v,c);
+            return K * x + B;
+        }
+
+        public override object ConvertBack(object v, Type t, object p, CultureInfo c)
+        {
+            if (v is null) return null;
+            var y = System.Convert.ToDouble(v, c);
+            return (y - B) / K;
+        }
+    }
+}
---
+++ EducationWPF/Infrastructure/Converters/Ratio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace EducationWPF.Infrastructure.Converters
+{
+    internal class Ratio :Converter
+    {
+        [ConstructorArgument("K")]
+        public double k { get; set; } = 1;
+
+        public Ratio()
+        {
+
+        }
+
+        public Ratio(double k)=>this.k = k;
+
+        public override object Convert(object value, Type t, object p, CultureInfo c)
+        {
+            if (value is null) return null;
+
+            var x= System.Convert.ToDouble(value, c);
+            return x * k;
+
+        }
+
+        public override object ConvertBack(object value, Type t, object p, CultureInfo c)
+        {
+            if (value is null) return null; var x = System.Convert.ToDouble(value, c);
+            return x / k;
+        }
+    }
+}
