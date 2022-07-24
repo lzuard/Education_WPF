610dcd342446bf180b9ac944dab1bfd055ac9d1c
Izuard <sasha.20133@gmail.com>
2022-07-24 14:05:40 +0300

    Add MultiValueConverter

---
+++ EducationWPF/Infrastructure/Converters/MultiValueConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EducationWPF.Infrastructure.Converters
+{
+    internal abstract class MultiValueConverter : IMultiValueConverter
+    {
+        public abstract object Convert(object[] vv, Type t, object p, CultureInfo c);
+
+        public virtual object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c) => 
+            throw new NotSupportedException();
+    }
+}
---
+++ EducationWPF/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EducationWPF.Infrastructure.Converters
+{
+    internal class ToArray : MultiValueConverter
+    {
+        public override object Convert(object[] vv, Type t, object p, CultureInfo c)
+        {
+            var collection = new CompositeCollection();
+            foreach (var value in vv)
+                collection.Add(value);
+            return collection;
+        }
+
+        //public override object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c) => v as object[];
+    }
+}
