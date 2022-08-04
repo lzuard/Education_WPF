a20a169563188523acda57ed66b3f42d6e06cd85
Izuard <sasha.20133@gmail.com>
2022-08-04 17:56:52 +0300

    some work with threads

--- Tests/EWPF_Console/Program.cs
+++ Tests/EWPF_Console/Program.cs
@@ -1,73 +1,35 @@
-﻿
-using System;
-using System.Globalization;
-
-namespace EWPF_Console // Note: actual namespace depends on the project name.
+﻿using System;
+using System.Threading;
+namespace EWPF_Console 
 {
     internal class Program
     {
-        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-
-
-        private static async Task<Stream> GetDataStream()
-        {
-            var client = new HttpClient();
-            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
-            return await response.Content.ReadAsStreamAsync();
-        }
-
-        private static IEnumerable<string> GetDataLines()
-        {
-            using var data_stream = GetDataStream().Result;
-            using var data_reader = new StreamReader(data_stream);
-
-            while (!data_reader.EndOfStream)
-            {
-                var line = data_reader.ReadLine();
-                if (string.IsNullOrWhiteSpace(line)) continue;
-                yield return line.Replace("Korea,", "Korea -").Replace("Bonaire,", "Bonaire -");
-            }
-        }
-
-        private static DateTime[] GetDates() => GetDataLines()
-            .First()
-            .Split(',')
-            .Skip(4)
-            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
-            .ToArray();
-
-        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
-        {
-            var lines = GetDataLines()
-                .Skip(1)
-                .Select(line => line.Split(','));
-
-            foreach (var row in lines)
-            {
-                var province=row[0].Trim() ?? String.Empty;
-                var country_name = row[1].Trim(' ', '"') ?? String.Empty;
-                var counts = row.Skip(4).Select(int.Parse).ToArray();
-                yield return (country_name, province, counts);
-            }
-
-        }
-
         static void Main(string[] args)
         {
-            //foreach (var data_line in GetDataLines())
-            //    Console.WriteLine(data_line);
+            Thread.CurrentThread.Name = "Main thread";
+            var thread = new Thread(ThreadMethod);
+            thread.Name = "Second thread";
+            thread.IsBackground = true;
+            thread.Priority=ThreadPriority.BelowNormal;
 
-            //var dates = GetDates();
-            
-            //Console.WriteLine(String.Join("\r\n", dates));
+            thread.Start(42);
 
-            //var russia_data = GetData()
-            //    .First(v=>v.Country.Equals("Russia",StringComparison.OrdinalIgnoreCase));
-            //Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia_data.Counts, (date, count) => $"{date:dd:MM} - {count}")));
-            //Console.ReadLine();
+            CheckThread();
+            Console.ReadLine();
+        }
 
-            double x = Double.Parse("31.22", CultureInfo.InvariantCulture);
-            Console.WriteLine(x);
+        private static void ThreadMethod(object parameter)
+        {
+            var value = (int)parameter;
+            Console.WriteLine(value);
+
+            CheckThread();
+        }
+
+        private static void CheckThread()
+        {
+            var thread = Thread.CurrentThread;
+            Console.WriteLine("Thread: "+thread.ManagedThreadId+" : "+thread.Name);
         }
     }
 }
