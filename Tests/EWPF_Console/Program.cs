9ae13c736eb785e96a33e489ac7d99ede24e5c96
Izuard <sasha.20133@gmail.com>
2022-08-04 18:02:50 +0300

    Send parametres into thread

--- Tests/EWPF_Console/Program.cs
+++ Tests/EWPF_Console/Program.cs
@@ -12,12 +12,39 @@ namespace EWPF_Console
             thread.IsBackground = true;
             thread.Priority=ThreadPriority.BelowNormal;
 
-            thread.Start(42);
+            thread.Start(42);  //first and bad method to start a thread
+
+
+            var count = 5;
+            var msg = "Hello World!";
+            var timeout = 150;
+
+            //second and good method to start a thread
+            new Thread(()=>PrintMethod(msg, count, timeout)) {IsBackground = true}.Start();
 
             CheckThread();
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Thread method that can't be started with Thread.Start()
+        /// </summary>
+        /// <param name="Message"></param>
+        /// <param name="Count"></param>
+        /// <param name="Timeout"></param>
+        private static void PrintMethod(string Message, int Count, int Timeout)
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                Console.WriteLine(Message);
+                Thread.Sleep(Timeout);
+            }
+        }
+
+        /// <summary>
+        /// Thread method that can be started with Thread.Start();
+        /// </summary>
+        /// <param name="parameter"></param>
         private static void ThreadMethod(object parameter)
         {
             var value = (int)parameter;
