7e962a1282923e11a65adbe43eacb01257eb4eb5
Izuard <sasha.20133@gmail.com>
2022-08-08 15:04:58 +0300

    Implement simple logic for browser interception

--- EducationWPF/Services/WebServerService.cs
+++ EducationWPF/Services/WebServerService.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EducationWPF.Services.Interfaces;
+using EWPF.WebLib;
 
 namespace EducationWPF.Services
 {
     internal class WebServerService : IWebServerService
     {
-        public bool Enabled { get; set; }
+        private WebServer _server = new WebServer(8080);
 
-        public void Start()
-        {
-            throw new NotImplementedException();
-        }
+        public bool Enabled { get=>_server.Enabled; set=>_server.Enabled=value; }
 
-        public void Stop()
+        public void Start() => _server.Start();
+
+        public void Stop() => _server.Stop();
+
+
+        public WebServerService() => _server.RequestReceived += OnRequestReceived;
+
+        private void OnRequestReceived(object? sender, RequestReceiverEventArgs e)
         {
-            throw new NotImplementedException();
+            using var writer = new StreamWriter(e.Context.Response.OutputStream);
+            writer.WriteLine("Application EWPF");
         }
     }
 }
